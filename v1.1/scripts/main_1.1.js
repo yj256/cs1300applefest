fe193bad89f353087cbdc6f7de9db04686f8d600
Mary <yj256@cornell.edu>
2016-12-07 16:09:58 -0500

    v1.1 of the website

---
+++ v1.1/scripts/main_1.1.js
@@ -0,0 +1,26 @@
+"use strict";
+// Sticky Header
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 80) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
+    }
+});
+
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
+    }
+});
